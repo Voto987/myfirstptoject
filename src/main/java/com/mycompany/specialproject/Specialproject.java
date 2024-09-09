535a7278651c88d8d432bd489cac070f561a40b4
RC_Student_lab <RC_Student_lab@10.0.0.227>
2024-09-09 08:17:14 +0000

    first commit

---
+++ src/main/java/com/mycompany/specialproject/Specialproject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.specialproject;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Specialproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
